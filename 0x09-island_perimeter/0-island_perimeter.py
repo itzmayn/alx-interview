bf7031df25c79e6e4e2ee4e0a2583f61904dcfc4
Mayn <123576452+itzmayn@users.noreply.github.com>
2024-03-14 05:51:28 +0000

    M

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,72 @@
 #!/usr/bin/python3
-"""Defines a function for finding the perimeter of an island."""
+"""
+Module defining the isWinner function.
 
-def island_perimeter(grid):
-    """Return the perimeter of an island.
+This module contains functions related to determining the winner in a prime game.
+"""
 
-    The function takes a 2D grid as input, where 0 represents water and 1 represents land.
-    It calculates the perimeter of the island defined in the grid based on the number
-    of land cells and shared edges between adjacent land cells.
+def isWinner(x, nums):
+    """
+    Determines the winner of a set of prime number removal games.
 
     Args:
-        grid (list): A list of lists of integers representing an island.
+        x (int): The number of rounds in the game.
+        nums (list of int): List of integers representing the number of prime
+                             numbers to be removed in each round.
 
     Returns:
-        int: The perimeter of the island defined in the grid.
+        str or None: The winner of the game ("Ben" or "Maria"), or None if the
+                     input is invalid.
     """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+    # Check for invalid input
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                if j > 0 and grid[i][j - 1] == 1:
-                    edges += 1
-                if i > 0 and grid[i - 1][j] == 1:
-                    edges += 1
-    return size * 4 - edges * 2
+    # Initialize scores for Ben and Maria
+    ben = 0
+    maria = 0
 
+    # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
+    # initialized to 1
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+
+    # The first two elements of the list, a[0] and a[1], are set to 0
+    # because 0 and 1 are not prime numbers
+    a[0], a[1] = 0, 0
+
+    # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    # Play each round of the game
+    for i in nums:
+        # If the sum of prime numbers in the set is even, Ben wins
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
+    # Determine the winner of the game
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """
+    Removes multiples of a prime number from an array of possible prime
+    numbers.
+
+    Args:
+        ls (list of int): The list of integers representing prime numbers.
+        x (int): The prime number whose multiples need to be removed from ls.
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
